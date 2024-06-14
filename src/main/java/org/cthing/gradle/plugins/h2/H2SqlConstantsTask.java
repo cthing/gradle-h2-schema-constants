f3a0232a83b81ded0d8ced79d551d35fd7c53585
Baron Roberts <baron@cthing.com>
2024-06-13 20:25:43 -0700

    Use SPDX license header.

--- src/main/java/org/cthing/gradle/plugins/h2/H2SqlConstantsTask.java
+++ src/main/java/org/cthing/gradle/plugins/h2/H2SqlConstantsTask.java
@@ -1,17 +1,6 @@
 /*
  * Copyright 2024 C Thing Software
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package org.cthing.gradle.plugins.h2;
@@ -242,7 +231,7 @@ public class H2SqlConstantsTask extends AbstractConstantsTask {
     @Override
     protected void loadDatabase(final Connection conn, final DataSource dataSource) throws SQLException, IOException {
         for (final File scriptFile : getSource().getFiles()) {
-            LOGGER.info("Loading the database with " + scriptFile);
+            LOGGER.info("Loading the database with {}", scriptFile);
             try (Reader reader = Files.newBufferedReader(scriptFile.toPath(), StandardCharsets.UTF_8)) {
                 RunScript.execute(conn, reader);
             }
